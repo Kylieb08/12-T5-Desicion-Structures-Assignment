41a12824cc7c18d55df51e4616ff1cde92a5ac23
339780520 <339780520@2260I-L29-008>
2025-09-22 09:21:33 -0400

    finished compass

--- Program.cs
+++ Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Compass();
+        }
+
+        public static void Compass()
+        {
+            int degree;
+
+            Console.WriteLine("Please enter a degree between 0 and 360");
+
+            while (!int.TryParse(Console.ReadLine(), out degree))
+            {
+                Console.WriteLine("Invalid input. Please try again");
+            }
+
+            if (degree < 0 || degree > 360)
+            {
+                if (degree < 0)
+                {
+                    degree = 360 + (degree % 360);
+                }
+
+                else
+                    degree = degree % 360;
+
+            }
+
+            Console.WriteLine($"You entered {degree} degrees");
         }
     }
 }
