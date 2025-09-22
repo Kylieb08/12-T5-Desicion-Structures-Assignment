e6c4539a3210ccf14843b7f8cac20303ea9af52f
339780520 <339780520@2260I-L29-008>
2025-09-22 09:08:04 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _12_T5_Desicion_Structures_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
