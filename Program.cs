589b8a152cddc7d95632d2e71fefe13ff12043cd
339780520 <339780520@2260I-L29-008>
2025-09-23 09:10:10 -0400

    done parking garage cost

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Compass();
+            ParkingGarageCost();
         }
 
         public static void Compass()
@@ -47,10 +48,41 @@
             else if (degree <= 225 && degree >= 136)
             {
                 Console.WriteLine("South");
-            }
+            }   
 
             else // <= 314 && >= 226
                 Console.WriteLine("West");
+        }
+
+        public static void ParkingGarageCost()
+        {
+            double hoursParked, totalCost, minutes;
+            
+
+            Console.WriteLine("How many minutes were you parked in the garage?");
+
+            while (!double.TryParse(Console.ReadLine(), out minutes) || minutes < 0)
+            {
+                Console.WriteLine("Invalid input. Please try again");
+            }
+
+            hoursParked = Math.Ceiling(minutes / 60);
+
+            if (hoursParked == 0)
+            {
+                Console.WriteLine("You did not park in this garage");
+            }
+
+            else if (hoursParked > 0)
+            {
+                totalCost = 2.00 + (hoursParked * 2);
+
+                if (totalCost > 20)
+                    totalCost = 20;
+
+                Console.WriteLine($"You were parked for {hoursParked} hour(s)");
+                Console.WriteLine($"Total parking cost: {totalCost.ToString("C")}");
+            }
         }
     }
 }
